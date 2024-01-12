2cfe1b2f7eaebc8c4ca60ff9f7cf311dcb63ec26
nazarnasim999 <nazarnasim101@gmail.com>
2024-01-12 20:53:47 +0500

    ///

--- controllers/vendor_customer_controllers/customer_controllers.js
+++ controllers/vendor_customer_controllers/customer_controllers.js
@@ -34,7 +34,7 @@ const create_customer = async (req, res, next) => {
             Name,
             email,
             password,
-            Profile_Image,
+            // Profile_Image,
             Home_Address,
             zipCode,
 
@@ -52,7 +52,7 @@ const create_customer = async (req, res, next) => {
         const store_user_data = {
             type,
             Name,
-            Profile_Image,
+            // Profile_Image,
             email,
             password: secure_password,
             Home_Address,
